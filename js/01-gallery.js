6142e638b8f1ffeb4514acd09da1939d24adcf30
BohdanaT <126656948+BohdanaT@users.noreply.github.com>
2023-07-23 22:26:25 +0300

    gallery-task-01-commit-01

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,20 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const galleryItemsRef = document.querySelector('.gallery');
+
+function createGalleryMarkup({ original, description }) {
+    const galleryElement = document.createElement('li');
+    galleryElement.classList.add('gallery__item');
+
+    const galleryImage = document.createElement('img');
+    galleryImage.classList.add('gallery__image');
+    galleryImage.src = original;
+    galleryImage.alt = description;
+
+    galleryElement.append(galleryImage);
+
+    return galleryElement;
+}
+
+const createGallery = galleryItems.map(createGalleryMarkup);
+galleryItemsRef.append(...createGallery);
