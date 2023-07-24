cbec915d5874151c9f51c70ac11c76edf80b6627
BohdanaT <126656948+BohdanaT@users.noreply.github.com>
2023-07-24 23:13:53 +0300

    gallery-task-01-commit-02

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,13 +2,13 @@ import { galleryItems } from './gallery-items.js';
 
 const galleryItemsRef = document.querySelector('.gallery');
 
-function createGalleryMarkup({ original, description }) {
+function createGalleryMarkup ({ preview, description }) {
     const galleryElement = document.createElement('li');
     galleryElement.classList.add('gallery__item');
 
     const galleryImage = document.createElement('img');
     galleryImage.classList.add('gallery__image');
-    galleryImage.src = original;
+    galleryImage.src = preview;
     galleryImage.alt = description;
 
     galleryElement.append(galleryImage);
@@ -18,3 +18,19 @@ function createGalleryMarkup({ original, description }) {
 
 const createGallery = galleryItems.map(createGalleryMarkup);
 galleryItemsRef.append(...createGallery);
+
+
+galleryItemsRef.addEventListener('click', onGalleryImageClick)
+
+function onGalleryImageClick(evt) {
+    const src = evt.target.original
+    
+    
+    
+    const instance = basicLightbox.create(`
+    <img src="https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820_1280.jpg"
+    width="1280">
+    `)
+
+    instance.show();
+}
